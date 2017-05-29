48d4d4acd43fa840ffd69105a2a9048436d61236
Apostolos Bessas <mpessas@gmail.com>
2017-05-29 19:23:42 +0300

    Fetch the SSH keys from all users in IAM
    We assume that the keys will be used for the ubuntu user.

---
+++ get_aws_keys.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+[ "$#" -ne 1 ] && exit -1
+[ "$1" != "ubuntu" ] && exit -1
+
+usernames=$(aws iam list-users --query "Users[*].[UserName]" --output=text)
+for username in $usernames; do
+    aws iam list-ssh-public-keys --user-name "$username" --query "SSHPublicKeys[?Status == 'Active'].[SSHPublicKeyId]" --output text | while read -r KeyId; do
+        aws iam get-ssh-public-key --user-name "$username" --ssh-public-key-id "$KeyId" --encoding SSH --query "SSHPublicKey.SSHPublicKeyBody" --output text
+    done
+done
